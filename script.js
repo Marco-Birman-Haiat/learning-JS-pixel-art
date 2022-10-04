716dc00ece6f7f022247dd13b3d26a8d95ddce38
Marco Haiat <marco.haiat@gmail.com>
2022-10-04 18:04:09 -0300

    Realiza algumas ações do requisito 5, ainda incompleto!!!

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 // Função que gera 3 valores entre 0 e 250 e retorna no formato de texto RGB para CSS
+function checkWhite (r, g, b) {
+    if (r == 0 && g == 0 && b == 0) {
+        return true
+    }
+    return false
+}
+
 function colorGenerator () {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
 
+    if(checkWhite(r, g, b)) {
+        colorGenerator()
+    }
     return `rgb(${r}, ${g}, ${b})`
 }
 
@@ -18,9 +28,19 @@ function checkRepeat (array) {
     return false
 }
 
+// Salva cores geradas no local storage
+function saveColorLC() {
+    let colorElements = document.getElementsByClassName('color')
+    let colors = [];
+    for (let index = 1; index < array.length; index += 1) {
+        colors.push(colorElements[index].style.backgroundColor);
+    }
+    localStorage.setItem('colorPalette', toString(colors))
+}
+
 // Itera sobre os elementos a serem coloridos e atribui as novas cores randomicas
 function generateRandomColors () {
-    colorElements = document.getElementsByClassName('color');
+    let colorElements = document.getElementsByClassName('color');
 
     for (let index = 1; index < colorElements.length; index += 1) {
         let elementSelected = colorElements[index];
@@ -28,9 +48,10 @@ function generateRandomColors () {
         elementSelected.style.backgroundColor = newCollor;
     }
     // Chama a função que verifica repetição e caso haja chama recursivamente a função atual
-    if (false){
+    if (checkRepeat(colorElements)) {
         generateRandomColors();
     }
+    saveColorLC()
 }
 
 generateRandomColors()
